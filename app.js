06525d7fa885a9183bc5b311780bb512113563f2
Farrel <farrelcodes@gmail.com>
2023-01-03 11:02:32 +0700

    Level 4 - Hashing with bcrypt and salting rounds

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -32,14 +34,14 @@ app.get("/login", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
+  
   User.findOne({email: username}, function(err, foundUser) {
     if (!err) {
-      if (foundUser.password === password) {
-        res.render("secrets");
-      } else {
-        res.send("Invalid email/password. Please try again.");
-      }
+      bcrypt.compare(password, foundUser.password, function(err, result) {
+        if (result === true) res.render("secrets");
+        else res.send("Invalid email/password. Please try again.");
+      });
     } else {
       res.send(err);
     }
@@ -51,15 +53,17 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
 
-  newUser.save(function(err) {
-    if (!err) {
-      res.render("secrets");
-    }
+    newUser.save(function(err) {
+      if (!err) {
+        res.render("secrets");
+      }
+    });
   });
 });
 
