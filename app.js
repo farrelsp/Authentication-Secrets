28de452059a7ad39e73f28e5c587d2df6b4e4a81
Farrel <farrelcodes@gmail.com>
2023-01-03 09:45:22 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+require('dotenv').config()
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const secret = process.env.SECRET;
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({email: username}, function(err, foundUser) {
+    if (!err) {
+      if (foundUser.password === password) {
+        res.render("secrets");
+      } else {
+        res.send("Invalid email/password. Please try again.");
+      }
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if (!err) {
+      res.render("secrets");
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000...");
+});
